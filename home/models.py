8f0b20e7ff8c54e523241d9cda0c1ba5dedfa36a
vaniakazeko <vania.kazeko.00@bk.ru>
2023-04-27 13:08:43 +0300

    add fields in HopePage

--- home/models.py
+++ home/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 from wagtail.models import Page
+from wagtail.admin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
-    pass
+    subtitle = models.CharField(max_length=250, blank=True, null=True, verbose_name='Подзаголовок')
+
+    content_panels = Page.content_panels + [FieldPanel('subtitle')]
+
+    # promote_panels = []
+    # settings_panels = []
