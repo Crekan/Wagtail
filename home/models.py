068b6d2123b867dffc52ec61735062c57d5a1845
vaniakazeko <vania.kazeko.00@bk.ru>
2023-04-27 13:22:26 +0300

    RichTextField connection

--- home/models.py
+++ home/models.py
@@ -2,12 +2,17 @@ from django.db import models
 
 from wagtail.models import Page
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.fields import RichTextField
 
 
 class HomePage(Page):
     subtitle = models.CharField(max_length=250, blank=True, null=True, verbose_name='Подзаголовок')
+    rtfbody = RichTextField(blank=True, null=True)
 
-    content_panels = Page.content_panels + [FieldPanel('subtitle')]
+    content_panels = Page.content_panels + [
+        FieldPanel('subtitle'),
+        FieldPanel('rtfbody'),
+    ]
 
     # promote_panels = []
     # settings_panels = []
